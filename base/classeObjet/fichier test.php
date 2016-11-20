299db46db81ee329028d5b613f1ce2cf4b86d7fc
Thomas <altum41@gmail.com>
2016-11-20 16:12:02 +0100

    test function

--- base/classeObjet/fichier test.php
+++ base/classeObjet/fichier test.php
@@ -1,1 +1,49 @@
-classe lololol
+<?php
+
+// classe de base, avec des propriétés et des méthodes membres
+class Vegetable {
+
+   var $edible;
+   var $color;
+
+   function Vegetable($edible, $color="green") 
+   {
+       $this->edible = $edible;
+       $this->color = $color;
+   }
+
+   function is_edible() 
+   {
+       return $this->edible;
+   }
+
+   function what_color() 
+   {
+       return $this->color;
+   }
+
+} // fin de la classe Vegetable
+
+// étend la classe de base
+class Spinach extends Vegetable {
+
+   var $cooked = false;
+
+   function Spinach() 
+   {
+       $this->Vegetable(true, "green");
+   }
+
+   function cook_it() 
+   {
+       $this->cooked = true;
+   }
+
+   function is_cooked() 
+   {
+       return $this->cooked;
+   }
+
+} // fin de la classe Spinach
+
+?>
